6465e338d75ded5c13461f7032451a67a4553511
ttom795 <69662271+ttom795@users.noreply.github.com>
2021-06-21 10:05:06 +1200

    Multiple sections, road concept

--- Assets/Generator.cs
+++ Assets/Generator.cs
@@ -3,32 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Generator : MonoBehaviour
 {
-    public int seed;
     public int xSize;
     public int ySize;
-    private Vector2 offsetFromZero;
-    private List<List<bool>> bool_array = new List<List<bool>>();
+    public List<GameObject> prefabsToSpawn = new List<GameObject>();
     public GameObject borderPrefab;
-    public int borderItemsPerEdge;
     public Quaternion borderRotationOffset = Quaternion.Euler(0, 90, 0);
-    public float borderSpacing = 4f;
-    public float separationCutoff = 3f;
-    public List<GameObject> prefab_list = new List<GameObject>();
+    public float borderSpacing = 4.5f;
+    public float minimumDistanceApart = 2.5f;
     private GameObject currentPrefab;
-    private List<Vector2> prefabLocations = new List<Vector2>();
-    public Material groundMat;
+    public Material groundMaterial;
     private List<Vector3> vertices = new List<Vector3>();
+    private List<Vector2> prefabLocations = new List<Vector2>();
+    private List<List<bool>> bool_array = new List<List<bool>>();
     public List<GameObject> borderInstances = new List<GameObject>();
 
-    void Start()
+    public void startGeneration(int seed)
     {
         Random.InitState(seed);
-        offsetFromZero = new Vector2(xSize / 2 - 0.5f, ySize / 2 - 0.5f);
         generateFilledBoolArray();
         generatePrefabs();
         generateBoundingGeometry();
         generateGroundPolygon();
         generateBorderPrefabs();
+        transform.position = Vector3.zero;
     }
 
     void generateFilledBoolArray()
@@ -45,33 +42,29 @@ public class Generator : MonoBehaviour
     }
     void generatePrefabs()
     {
-        
-        foreach (GameObject item in prefab_list){
-            int h = Random.Range(0, ySize);
-            int w = Random.Range(0, xSize);
-            float new_h = h - offsetFromZero.y;
-            float new_w = w - offsetFromZero.x;
+        foreach (GameObject item in prefabsToSpawn){
+            int h = Random.Range(0, ySize-1);
+            int w = Random.Range(0, xSize-1);
             currentPrefab = item;
-            if (bool_array[h][w] == false)
-            {
+            if (bool_array[h][w] == false){
                 bool_array[h][w] = true;
-                int prefabX = currentPrefab.GetComponent<GeneratedItem>().xSize + 1;             //+1 to exclude prefab 0,0 location - adds border.
-                int prefabY = currentPrefab.GetComponent<GeneratedItem>().ySize + 1;             //Same for Y
-                bool generateBorder = currentPrefab.GetComponent<GeneratedItem>().generateBorder;
+                int prefabX = currentPrefab.GetComponent<GeneratedItem>().xSize + 1;
+                int prefabY = currentPrefab.GetComponent<GeneratedItem>().ySize + 1;
                 for (int y = -prefabY + 1; y < prefabY; y++){
                     for (int x = -prefabX + 1; x < prefabX; x++){
                         if (h + y >= 0 && h + y < ySize && w + x >= 0 && w + x < xSize){
                             bool_array[h + y][w + x] = true;
-                            
                         }
                     }
                 }
-                Quaternion rotation = Quaternion.Euler(0f, Random.Range(0, 360), 0f);
-                Instantiate(currentPrefab, new Vector3(new_h, 0.5f, new_w), rotation);
-                prefabLocations.Add(new Vector2(new_h + prefabY, new_w - prefabX));
-                prefabLocations.Add(new Vector2(new_h - prefabY, new_w + prefabX));
-                prefabLocations.Add(new Vector2(new_h + prefabY, new_w + prefabX));
-                prefabLocations.Add(new Vector2(new_h - prefabY, new_w - prefabX));
+                Quaternion rotation = Quaternion.Euler(0f, Random.Range(0, 360-1), 0f);
+                float new_w = w + xSize / 2 + transform.position.x;
+                float new_h = h + ySize / 2 + transform.position.z;
+                Instantiate(currentPrefab, new Vector3(new_w, 0.5f, new_h), rotation);
+                prefabLocations.Add(new Vector2(new_w - prefabX, new_h + prefabY));
+                prefabLocations.Add(new Vector2(new_w + prefabX, new_h - prefabY));
+                prefabLocations.Add(new Vector2(new_w + prefabX, new_h + prefabY));
+                prefabLocations.Add(new Vector2(new_w - prefabX, new_h - prefabY));
             }
         }
     }
@@ -80,6 +73,7 @@ public class Generator : MonoBehaviour
     {
         Vector2[] points = prefabLocations.ToArray();
         HashSet<Vector2> result = new HashSet<Vector2>();
+        List<Vector2> collinearPoints = new List<Vector2>();
         int leftMostIndex = 0;
         for (int i = 1; i < points.Length; i++)
         {
@@ -87,7 +81,6 @@ public class Generator : MonoBehaviour
                 leftMostIndex = i;
         }
         result.Add(points[leftMostIndex]);
-        List<Vector2> collinearPoints = new List<Vector2>();
         Vector2 current = points[leftMostIndex];
         while (true)
         {
@@ -142,28 +135,28 @@ public class Generator : MonoBehaviour
         msh.RecalculateBounds();
         MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
         MeshFilter filter = gameObject.AddComponent<MeshFilter>();
-        renderer.material = groundMat;
+        renderer.material = groundMaterial;
         filter.mesh = msh;
     }
     void generateBorderPrefabs()
     {
         vertices.Add(vertices[0]);
-        for (int i = 0; i < vertices.Count-1; i++)
+        for (int i = 0; i < vertices.Count - 1; i++)
         {
             Vector2 p1 = new Vector2(vertices[i].x, vertices[i].z);
-            Vector2 p2 = new Vector2(vertices[i+1].x, vertices[i+1].z);
+            Vector2 p2 = new Vector2(vertices[i + 1].x, vertices[i + 1].z);
             float distance = Vector2.Distance(p1, p2);
-            
+
             for (float x = 0; x < distance; x += borderSpacing)
             {
                 float value = x / distance;
                 Vector2 pos = Vector2.Lerp(p1, p2, value);
                 Vector3 pos3d = new Vector3(pos.x, 0f, pos.y);
-                Quaternion rotation = Quaternion.LookRotation((vertices[i + 1] - pos3d).normalized)*borderRotationOffset;
+                Quaternion rotation = Quaternion.LookRotation((vertices[i + 1] - pos3d).normalized) * borderRotationOffset;
                 bool toggle = true;
                 foreach (GameObject item in borderInstances)
                 {
-                    if (Vector3.Distance(item.transform.position, pos3d) < separationCutoff)
+                    if (Vector3.Distance(item.transform.position, pos3d) < minimumDistanceApart)
                     {
                         toggle = false;
                     }
